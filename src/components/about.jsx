2dab5455a3ee337afb1f455cca4bc586b3b6a0ef
CKPRASHANTH07 <ckprashanthc@gmail.com>
2023-07-29 11:36:30 +0530

    after about

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -1,15 +1,23 @@
-// About.js
-
 import React from 'react';
 
 const About = () => {
   return (
     <div className='relative z-0'>
-      <div className='hidden md:flex flex-col absolute container left-10 md:left-20 w-full text-xl md:text-4xl text-center py-20 md:py-[908px] px-4 md:px-[10px]'>
-        <h1 className='text-slate-950 font-poppins text-4xl md:text-4xl py-10'>ABOUT ME</h1>
-        <h1 className='text-zinc-500 font-serif animate-pulse md:text-xl'>
-          I am seeking a competitive and challenging environment where I can serve your organization and establish a career for myself. With a considerable track record of successfully collaborating on diverse projects and participating in challenging competitions, I have developed a strong aptitude for thriving in high-pressure environments.
-        </h1>
+       <div className='flex md:hidden '>
+        <div className='absolute left-5 top-96 bottom-10 '>
+          <h1 className='text-slate-950 font-poppins text-3xl '>About Me</h1>
+          <h1 className='text-zinc-500 font-serif animate-pulse text-xl'>I am seeking a competitive and challenging environment where I can serve your organization and establish a career for myself.</h1>
+          <h1 className='text-zinc-500 font-serif animate-pulse text-xl'> With a considerable track record of successfully collaborating on diverse projects and participating in challenging competitions,</h1>
+          <h1 className='text-zinc-500 font-serif animate-pulse text-xl'> I have developed a strong aptitude for thriving in high-pressure environments.</h1>
+        </div>
+      </div>
+      <div className='hidden md:flex'>
+        <div className='absolute  top-[700px]  w-full text-5xl  px-80'>
+          <h1 className='text-slate-950 font-poppins text-7xl '>About Me</h1>
+          <h1 className='text-zinc-500 font-serif animate-pulse text-xl'>I am seeking a competitive and challenging environment where I can serve your organization and establish a career for myself.</h1>
+          <h1 className='text-zinc-500 font-serif animate-pulse text-xl'> With a considerable track record of successfully collaborating on diverse projects and participating in challenging competitions,</h1>
+          <h1 className='text-zinc-500 font-serif animate-pulse text-xl'> I have developed a strong aptitude for thriving in high-pressure environments.</h1>
+        </div>
       </div>
     </div>
   );
--- src/components/home.jsx
+++ src/components/home.jsx
@@ -5,18 +5,18 @@ import React from 'react';
 const Home = () => {
   return (
     <div className='relative z-0'>
-      <div className='flex md:hidden py-8 md:py-16'>
-        <div className='absolute left-5 top-20 md:top-40'>
-          <h1 className='text-zinc-500 font-serif text-xl md:text-2xl'>Hello, I am</h1>
-          <h1 className='text-slate-950 font-poppins text-4xl md:text-5xl'>KEERTHI PRASHANTH C</h1>
-          <h1 className='text-zinc-500 font-serif animate-pulse md:text-xl'>I'm a Full Stack Web Developer,</h1>
-          <h1 className='text-zinc-500 font-serif animate-pulse md:text-xl'>ECE Student,</h1>
-          <h1 className='text-zinc-500 font-serif animate-pulse md:text-xl'>Auto-Mobile &amp; Tech Enthusiast.</h1>
+      <div className='flex md:hidden '>
+        <div className='absolute left-5 top-[110px] '>
+          <h1 className='text-zinc-500 font-serif text-xl '>Hello, I am</h1>
+          <h1 className='text-slate-950 font-poppins text-3xl '>KEERTHI-- PRASHANTH C</h1>
+          <h1 className='text-zinc-500 font-serif animate-pulse text-xl'>I'm a Full Stack Web Developer,</h1>
+          <h1 className='text-zinc-500 font-serif animate-pulse text-xl'>ECE Student,</h1>
+          <h1 className='text-zinc-500 font-serif animate-pulse text-xl'>Auto-Mobile &amp; Tech Enthusiast.</h1>
         </div>
       </div>
 
-      <div className='hidden md:flex w-full text-5xl text-center px-4 md:px-80'>
-        <div className='absolute text-center top-56 content-center'>
+      <div className='hidden md:flex w-full text-5xl  px-80'>
+        <div className='absolute  top-56 '>
           <h1 className='text-zinc-500 font-serif text-5xl animate-pulse'>Hello, I am</h1>
           <h1 className='text-slate-950 font-poppins text-7xl'>KEERTHI PRASHANTH C</h1>
           <h1 className='text-zinc-500 font-serif animate-pulse text-xl'>I'm a Full Stack Web Developer,</h1>
