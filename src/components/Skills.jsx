4a777da9ed0264ff8cec953515fa4247d691ad8a
CKPRASHANTH07 <ckprashanthc@gmail.com>
2023-08-06 17:53:06 +0530

    b4 skills some fixes

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -4,8 +4,18 @@ const Skills = () => {
   return (
 
     <div className='absolute  top-[3100px]  w-full  text-center  font-poppins  justify-center '>
-        <h1 className='text-7xl text-bold'>Skills</h1>
         
+        <div className='space-y-10'>
+        <h1 className='text-7xl text-bold'>Skills</h1>
+        <button className='text-xl flex-col text-center  p-10  bg-slate-200 rounded-2xl shadow-black shadow-2xl'>
+<img className='object scale-50' src="logo" alt="" />
+         <h1>Communication</h1>
+         <h1>Problem Solving</h1>
+         <h1>Creativity</h1>
+         <h1>Collaborative</h1> 
+
+        </button>
+        </div>
         <div></div>
     
     
