c95110bb1c2295c31dc209fcd74f7c4bd9dc25e0
chanleeip <chanleeip@gmail.com>
2023-09-12 22:21:41 +0530

    commited registeration page-images

--- src/components/registration.jsx
+++ src/components/registration.jsx
@@ -4,10 +4,10 @@ import React from 'react';
 const Reg = () => {
   return (
     <div className="w-96 h-96 relative bg-white">
-  <input className="w-80 h-12 left-[28px] top-[155.60px] absolute bg-white rounded-lg border text-2xl text-center border-neutral-200" />
+  <input className="w-80 h-11 left-[28px] top-[155.60px] absolute bg-white rounded-lg border text-2xl text-center border-neutral-200" />
   <div className="w-16 h-16 left-[301.81px] top-[144px] absolute">
     <div className="w-16 h-16 left-0 top-0 absolute bg-neutral-200 rounded-full border border-neutral-200" />
-    <img className="w-9 h-12 left-[15.04px] top-[8.23px] absolute" src="https://via.placeholder.com/36x47" />
+    <img className="w-9 h-12 left-[15.04px] top-[8.23px] absolute" src="https://drive.google.com/uc?export=view&id=1kuktAXQXulyWwHkcC64Per1zxp22PNvB" alt="company logo" />
   </div>
   <div className="w-40 h-12 left-[42.04px] top-[130px] absolute">
     <div className="w-28 h-6 left-[5.19px] top-0 absolute bg-white" />
@@ -16,7 +16,7 @@ const Reg = () => {
   <div className="w-80 h-16 left-[28px] top-[347px] absolute">
     <input className="w-80 h-11 left-0 top-[17px] absolute bg-white rounded-lg border text-center text-2xl border-neutral-200" />
     <div className="w-16 h-16 left-[273.23px] top-[6px] absolute bg-neutral-200 rounded-full border border-neutral-200" />
-    <img className="w-9 h-8 left-[287.24px] top-[23px] absolute" src="https://via.placeholder.com/37x32" />
+    <img className="w-9 h-8 left-[287.24px] top-[23px] absolute" src="https://drive.google.com/uc?export=view&id=1kkKTnKIIGWwcVillOedwlBeLhS-VicDQ" />
     <div className="w-28 h-6 left-[14.53px] top-0 absolute">
       <div className="w-28 h-6 left-[5.19px] top-0 absolute bg-white" />
       <div className="w-28 h-5 left-0 top-[5px] absolute text-center text-black text-base font-semibold">Profession</div>
@@ -25,7 +25,7 @@ const Reg = () => {
   <div className="w-80 h-16 left-[28px] top-[455px] absolute">
     <div className="w-80 h-16 left-0 top-0 absolute">
       <input className="w-80 h-11 left-0 top-[11px] absolute bg-white rounded-lg border text-center text-2xl border-neutral-200" />
-      <img className="w-9 h-8 left-[289.50px] top-[18px] absolute" src="https://via.placeholder.com/37x32" />
+      <img className="w-9 h-8 left-[289.50px] top-[18px] absolute" src="https://drive.google.com/uc?export=view&id=1rLLol3_6UZGONSXTSLjv54J_OFMwzKrR" />
     </div>
     <div className="w-24 h-4 left-[22.91px] top-0 absolute">
       <div className="w-24 h-3.5 left-0 top-0 absolute bg-white" />
@@ -39,7 +39,7 @@ const Reg = () => {
       <div className="w-28 h-6 left-0 top-0 absolute bg-white" />
       <div className="w-28 h-6 left-[3.55px] top-0 absolute text-center text-black text-base font-semibold">User name</div>
     </div>
-    <img className="w-9 h-8 left-[288.30px] top-[17px] absolute" src="https://via.placeholder.com/37x33" />
+    <img className="w-9 h-8 left-[288.30px] top-[17px] absolute" src="https://drive.google.com/uc?export=view&id=1rQ0Pwc_z9EpBSYwpUXLfDbUtlK7udKn9" />
   </div>
   <div className="w-52 h-12 left-[98px] top-[44px] absolute">
     <div className="w-52 h-6 left-0 top-0 absolute text-sky-950 text-3xl font-bold">REGISTRATION</div>
@@ -50,7 +50,7 @@ const Reg = () => {
       <div className="w-48 h-12 left-0 top-0 absolute bg-sky-950 rounded-lg" />
       <div className="w-24 h-8 left-[34px] top-[6.12px] absolute text-white text-2xl font-bold">Create</div>
     </button>
-    <img className="w-9 h-11 left-[133px] top-0 absolute" src="https://via.placeholder.com/38x44" />
+    <img className="w-9 h-11 left-[133px] top-0 absolute" src="https://drive.google.com/uc?export=view&id=1BCVrpmCbXknfuudvdzDw01yYQlpOsAqL" />
   </button>
   <button className="w-80 h-6 left-[38.98px] top-[651px] absolute">
     <div className="w-40 h-6 left-[85.02px] top-0 absolute text-stone-300 text-lg font-bold">Already a user..?</div>
@@ -60,7 +60,7 @@ const Reg = () => {
       <div className="w-48 h-12 left-0 top-0 absolute bg-green-300 rounded-lg" />
       <div className="w-20 h-9 left-[33.07px] top-[8.16px] absolute text-white text-2xl font-bold">Login</div>
     </div>
-    <img className="w-9 h-11 left-[139px] top-[4px] absolute" src="https://via.placeholder.com/38x44" />
+    <img className="w-9 h-11 left-[139px] top-[4px] absolute" src="https://drive.google.com/uc?export=view&id=1KAkPqpibrFzOPjMs59A9KUaJEd5RFWeB" />
   </button>
 </div>
     
